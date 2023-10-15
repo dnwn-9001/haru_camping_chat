117c21b36356831151386e1097b34d28b7ef3f31
dnwn-9001 <j10511@hanmail.net>
2023-10-15 18:17:39 +0900

    fix:Hydration errors 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
-import "antd/dist/antd.css";
+// import "antd/dist/antd.css";
 import wrapper from "../store/configureStore";
 
-const App = ({ Component }) => {
+interface Prop {
+  Component: () => React.JSX.Element;
+}
+
+const App = ({ Component }: Prop) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <>
       <Head>
@@ -14,10 +23,6 @@ const App = ({ Component }) => {
       <Component />
     </>
   );
-};
-
-App.propTypes = {
-  Component: PropTypes.elementType.isRequired,
 };
 
 export default wrapper.withRedux(App);
