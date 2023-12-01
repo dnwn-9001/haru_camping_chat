38246b30eb64f2f2f48191a0858c3159fec4f667
dnwn-9001 <j10511@hanmail.net>
2023-12-01 22:38:44 +0900

    refactor:메인화면 상단 Navbar 표시 및 고정되게 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,20 @@
-import type { Metadata } from "next";
+"use client";
 import { Inter } from "next/font/google";
+import Link from "next/link";
+import styled from "styled-components";
+import { Avatar, Switch, Row, Col } from "antd";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Haru Camping",
-  description: "",
-};
+const Body = styled.body`
+  margin: 0;
+  height: 100vh;
+`;
+
+const NavBar = styled.div`
+  position: sticky;
+  top: 0px;
+`;
 
 export default function RootLayout({
   children,
@@ -15,7 +23,82 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <title>Haru Camping</title>
+      </head>
+      <Body className={inter.className}>
+        <NavBar>
+          <Row>
+            <Col
+              xs={2}
+              md={2}
+              style={{
+                margin: "auto",
+                paddingLeft: "20px",
+              }}
+            >
+              <div>
+                <Switch defaultChecked onChange={onChange} />
+              </div>
+            </Col>
+            <Col
+              xs={20}
+              md={20}
+              style={{
+                marginTop: "14px",
+                textAlign: "center",
+              }}
+            >
+              <div>
+                <Link href="/">
+                  <img
+                    src="/images/logo.png"
+                    alt="하루캠핑 로고"
+                    style={{ width: "130px" }}
+                  />
+                </Link>
+              </div>
+            </Col>
+
+            <Col
+              xs={2}
+              md={2}
+              style={{
+                margin: "auto",
+                textAlign: "right",
+                paddingRight: "20px",
+              }}
+            >
+              {/* {isLoggedIn ? (
+            <Link href="/login">
+                <Avatar
+                  icon={<UserOutlined />}
+                  style={{
+                    backgroundColor: "#87d068",
+                  }}
+                />
+        
+            </Link>
+          ) : (
+            <Link href="/profile">
+            
+                <Avatar
+                  icon={<UserOutlined />}
+                  style={{
+                    backgroundColor: "#87d068",
+                  }}
+                />
+            </Link>
+          )} */}
+            </Col>
+          </Row>
+        </NavBar>
+        <Row>{children}</Row>
+      </Body>
     </html>
   );
 }
+
+const onChange = (checked: boolean) => {
+  console.log(`switch to ${checked}`);
+};
