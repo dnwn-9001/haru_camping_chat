6620d4950dc2140feea2c7e02c86837cfa132aad
dnwn-9001 <j10511@hanmail.net>
2024-01-03 20:37:34 +0900

    feat: supabase로 카카오,구글 OAuth Sign in 구현

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,32 +1,15 @@
-import React, { useState, useCallback, ChangeEvent, useEffect } from "react";
-import { Button, Form, Input } from "antd";
-import Link from "next/link";
+import React, { useRef } from "react";
 import styled from "styled-components";
-import { useDispatch } from "react-redux";
-import { loginAction } from "../reducers";
+import Image from "next/image";
+import { supabase } from "@/lib/supabase";
+import { makeStore, AppStore } from "../store/store";
+import { useAppSelector, useAppDispatch, useAppStore } from "../store/hooks";
 
 const LoginTitle = styled.h1`
   margin: auto;
   width: 300px;
   text-align: center;
   padding: 100px 0 50px 0;
-`;
-
-const InputWrapper = styled.div`
-  margin: auto;
-  margin-bottom: 20px;
-  width: 300px;
-`;
-
-const LoginButton = styled(Button)`
-  background-color: rgba(50, 92, 62, 0.5);
-  border: none;
-  width: 100%;
-`;
-
-const SignupBtn = styled.a`
-  color: grey;
-  padding-top: 15px;
 `;
 
 const ButtonWrapper = styled.div`
@@ -37,55 +20,44 @@ const ButtonWrapper = styled.div`
   align-items: center;
 `;
 
+const GoogleBtnImg = styled(Image)`
+  margin-top: 20px;
+`;
+
 const LoginForm = () => {
-  // const dispatch = useDispatch();
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
+  async function signInWithKakao() {
+    await supabase.auth.signInWithOAuth({
+      provider: "kakao",
+    });
+  }
 
-  const onChangeId = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setId(e.target.value);
-  }, []);
-  const onChangePwd = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  }, []);
-
-  const onSubmitForm = useCallback(() => {
-    console.log(id, password);
-    // setIsLoggedIn(true);
-    // dispatch(loginAction({ id, password }));
-  }, [id, password]);
+  async function signInWithGoogle() {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+  }
 
   return (
     <>
       <LoginTitle>로그인</LoginTitle>
-      <Form onFinish={onSubmitForm}>
-        <InputWrapper>
-          <Input
-            name="user-id"
-            value={id}
-            onChange={onChangeId}
-            placeholder="아이디를 입력해주세요."
-            size="large"
-            required
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Input.Password
-            name="user-password"
-            value={password}
-            onChange={onChangePwd}
-            placeholder="비밀번호를 입력해주세요."
-            size="large"
-            required
-          />
-        </InputWrapper>
-        <ButtonWrapper>
-          <LoginButton type="primary" htmlType="submit" loading={false}>
-            로그인
-          </LoginButton>
-          <Link href="/signup">회원가입</Link>
-        </ButtonWrapper>
-      </Form>
+      <ButtonWrapper>
+        <Image
+          src="/images/kakao_login_large_wide.png"
+          width={300}
+          height={60}
+          alt="Sign in with KaKao"
+          style={{ cursor: "pointer" }}
+          onClick={signInWithKakao}
+        />
+        <GoogleBtnImg
+          src="/images/web_light_sq_SI@3x.png"
+          width={300}
+          height={60}
+          alt="Sign in with Google"
+          style={{ cursor: "pointer" }}
+          onClick={signInWithGoogle}
+        />
+      </ButtonWrapper>
     </>
   );
 };
