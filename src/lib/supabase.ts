261915152e6697378dc542ea625e192e94ab1095
dnwn-9001 <j10511@hanmail.net>
2024-01-05 02:24:15 +0900

    refactor: supabaseUrl,supabaseKey 환경변수로 접근하도록 수정

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseUrl: string = "https://vifbaselokneezcalqdb.supabase.co";
-const supabaseKey: string =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpZmJhc2Vsb2tuZWV6Y2FscWRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM4NjA0MDQsImV4cCI6MjAxOTQzNjQwNH0.seSu2UsNpRJn9l29OZe6K0IOJHFkoLrdA0Ssyr1ZldA";
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
