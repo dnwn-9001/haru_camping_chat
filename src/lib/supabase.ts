59809c341301b31987fa42c96dde1f72dc547042
dnwn-9001 <j10511@hanmail.net>
2024-01-12 21:12:10 +0900

    feat: OAuth provider token 셋팅 로직 추가

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,4 +5,22 @@ const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
+supabase.auth.onAuthStateChange((event, session) => {
+  if (session && session.provider_token) {
+    window.localStorage.setItem("oauth_provider_token", session.provider_token);
+  }
+
+  if (session && session.provider_refresh_token) {
+    window.localStorage.setItem(
+      "oauth_provider_refresh_token",
+      session.provider_refresh_token
+    );
+  }
+
+  if (event === "SIGNED_OUT") {
+    window.localStorage.removeItem("oauth_provider_token");
+    window.localStorage.removeItem("oauth_provider_refresh_token");
+  }
+});
+
 export { supabase };
