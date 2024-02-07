673d18a318f3bfbe4dfa291774cda6fae022ad2d
dnwn-9001 <j10511@hanmail.net>
2024-02-08 01:21:25 +0900

    feat: 채팅방 변경 기능&선택된 지역 배경색 변경

--- components/chat/AreaCard.tsx
+++ components/chat/AreaCard.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import axios from "axios";
 import { setArea } from "@/store/features/chat/areaSlice";
@@ -15,6 +15,22 @@ const AreaCard = ({ apiUrl }: ChatInputProps) => {
   const dispatch = useAppDispatch();
   const { socket } = useSocket();
   const { full_name, email } = useAppSelector((state) => state.user);
+  const { area } = useAppSelector((state) => state.area);
+  const [areaList, setAreaList] = useState<string[]>([]);
+
+  useEffect(() => {
+    setAreaList([
+      "서울",
+      "인천",
+      "경기도",
+      "부산",
+      "강원도",
+      "충청도",
+      "전라도",
+      "경상도",
+      "제주",
+    ]);
+  }, []);
 
   useEffect(() => {
     if (!socket) return;
@@ -41,39 +57,54 @@ const AreaCard = ({ apiUrl }: ChatInputProps) => {
 
   const onClickArea = (e: React.MouseEvent<HTMLDivElement>) => {
     const { nodeName, innerText } = e.target as HTMLDivElement;
-    if (nodeName === "H3") {
+
+    const dispatchAndPost = async () => {
       dispatch(setArea(innerText));
-      axios.post(apiUrl, {
+      await axios.post(apiUrl, {
         area: innerText,
         userName: full_name,
         userEmail: email,
       });
+    };
 
-      dispatch(initializeMsgList());
+    if (nodeName === "H3") {
+      if (area !== "") {
+        if (area === innerText) {
+          alert("이미 입장한 방입니다.");
+          return;
+        }
+        const result = confirm(
+          `${innerText} 채팅방으로 입장하시겠습니까? \n현재 채팅방은 퇴장 처리됩니다. \n퇴장 처리된 방의 채팅 내역은 모두 삭제됩니다.`
+        );
+        if (result) {
+          dispatch(initializeMsgList());
+          dispatchAndPost();
+        } else return;
+      } else {
+        dispatchAndPost();
+      }
     }
   };
 
   return (
     <div onClick={onClickArea}>
-      <AreaName>서울</AreaName>
-      <AreaName>인천</AreaName>
-      <AreaName>경기도</AreaName>
-      <AreaName>부산</AreaName>
-      <AreaName>강원도</AreaName>
-      <AreaName>충청도</AreaName>
-      <AreaName>전라도</AreaName>
-      <AreaName>경상도</AreaName>
-      <AreaName>제주</AreaName>
+      {areaList.map((name, i) => (
+        <AreaName key={`${i}_area`} selected={area === name}>
+          {name}
+        </AreaName>
+      ))}
     </div>
   );
 };
 
-const AreaName = styled.h3`
+const AreaName = styled.h3<{ selected?: boolean }>`
   padding: 10px;
   margin: 5px 0;
   border-radius: 10px;
+  background-color: ${({ selected }) => (selected ? "#fbfcfc" : "transparent")};
+  transition: background-color 0.5s ease;
   &:hover {
-    background: #fbfcfc;
+    background-color: #fbfcfc;
     cursor: pointer;
   }
 `;
